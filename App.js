16a6834d1537de9726cdc62d7b1149c48bc101d2
dikodigital <dikopenpro@gmail.com>
2026-05-15 20:36:33 +0100

    add app code

---
+++ App.js
@@ -0,0 +1,113 @@
+<!DOCTYPE html><html><head><meta charSet="utf-8"/><meta http-equiv="X-UA-Compatible" content="IE=edge"/><meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1"/><title>supportive green raspberries - Snack</title><meta name="description" content="Try this project on your phone! Use Expo&#x27;s online editor to make changes and save your own copy."/><meta property="og:url" content="https://snack.expo.dev/@diko1987/dikodigital-academy-%2Fraw%2FApp.js"/><meta property="og:title" content="supportive green raspberries - Snack"/><meta property="og:description" content="Try this project on your phone! Use Expo&#x27;s online editor to make changes and save your own copy."/><meta property="og:type" content="website"/><meta property="og:image" content="https://s3.amazonaws.com/exp-brand-assets/SnackIcon_200.png"/><meta property="og:image:width" content="200"/><meta property="og:image:height" content="200"/><meta name="twitter:card" content="summary"/><meta name="twitter:site" content="@expo"/><meta name="twitter:title" content="supportive green raspberries - Snack"/><meta name="twitter:description" content="Try this project on your phone! Use Expo&#x27;s online editor to make changes and save your own copy."/><meta name="twitter:image" content="https://s3.amazonaws.com/exp-brand-assets/SnackIcon_200.png"/><meta name="robots" content="none"/><meta name="google-site-verification" content="Fh25fNM-buRYptb-TO6MVgOjXGzdhmAnRgIC7sdrmbw"/><link rel="shortcut icon" href="/favicon.ico"/><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,500,600"/><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.min.css"/><style type="text/css">
+              :root {
+                --font-normal: 'Source Sans Pro', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+                --font-monospace: 'SF Mono', Monaco, Consolas, 'Liberation Mono', 'Courier New',
+                  monospace;
+              }
+
+              html {
+                box-sizing: border-box;
+              }
+
+              *,
+              *:before,
+              *:after {
+                box-sizing: inherit;
+              }
+
+              *:focus {
+                outline: none;
+              }
+
+              *:focus-visible {
+                outline: auto;
+              }
+
+              html,
+              body {
+                height: 100%;
+                width: 100%;
+                overflow: hidden;
+              }
+
+              body {
+                font-family: var(--font-normal);
+                font-size: 14px;
+                line-height: 1.42857143;
+                overscroll-behavior: none;
+              }
+
+              div {
+                scrollbar-width: thin;
+                scrollbar-color: var(--color-disabled) var(--color-background);
+              }
+
+              @media (hover) {
+                ::-webkit-scrollbar {
+                  width: 12px;
+                  height: 12px;
+                  background: var(--color-background);
+                }
+                ::-webkit-scrollbar-thumb {
+                  background: var(--color-disabled);
+                  border-radius: 10px;
+                  border: 3px var(--color-background) solid;
+                }
+              }
+
+              button,
+              input,
+              select,
+              textarea {
+                font: inherit;
+                color: inherit;
+                line-height: inherit;
+              }
+
+              button {
+                cursor: pointer;
+              }
+
+              button[disabled] {
+                cursor: default;
+              }
+
+              #root {
+                height: 100vh;
+              }
+
+              a {
+                color: #4099ff;
+              }
+            </style><style type="text/css" data-aphrodite="true">._5qz8rht{height:100% !important;width:100% !important;--color-primary:#0081f1 !important;--color-secondary:#687076 !important;--color-error:#cd2b31 !important;--color-warning:#946800 !important;--color-success:#299764 !important;--color-primary-text:#ffffff !important;--color-secondary-text:#ffffff !important;--color-error-text:#ffffff !important;--color-warning-text:#ffffff !important;--color-success-text:#ffffff !important;--color-text:#11181c !important;--color-soft:#687076 !important;--color-soft-text:#ffffff !important;--color-background:#fbfcfd !important;--color-content:#ffffff !important;--color-hover:#f8f9fa !important;--color-disabled:#fbfcfd !important;--color-selected:#e6e8eb !important;--color-selected-text:#ffffff !important;--color-border:#d7dbdf !important;--color-border-editor:#dfe3e6 !important;--shadow-popover:0 15px 25px rgba(0, 0, 0, 0.12), 0 5px 10px rgba(0, 0, 0, 0.05) !important;--shadow-small:0 3px 6px rgba(0, 0, 0, 0.08), 0 2px 4px rgba(0, 0, 0, 0.07) !important;}._ld0hy8{-webkit-box-pack:center !important;-ms-flex-pack:center !important;-webkit-box-align:center !important;-ms-flex-align:center !important;height:100% !important;width:100% !important;display:-webkit-box !important;display:-moz-box !important;display:-ms-flexbox !important;display:-webkit-flex !important;display:flex !important;-webkit-align-items:center !important;align-items:center !important;-webkit-justify-content:center !important;justify-content:center !important;padding:24px !important;}._1mvr6gm{padding-left:24px !important;border-left:1px solid #eee !important;}._16pg94n{margin:0px !important;}._166ju6w{margin-bottom:0px !important;}</style><script>
+               if (window.location.search.indexOf('hideQueryParams=true') >= 0) {
+                 history.replaceState(null, document.title, window.location.pathname);
+               }
+               window.__INITIAL_DATA__ = {"data":{"type":"error","error":{"message":"Server returned errors when fetching data"},"defaults":{"name":"supportive green raspberries","channel":"rufDCcYC0e"}},"splitTestSettings":{"defaultConnectionMethod":"account","authFlow":"save1","snackFirstSeen":"2026-05-15"},"queryParams":{},"postData":{}} </script></head><body><div id="root"><div class="_5qz8rht"><div class="_ld0hy8"><div class="_1mvr6gm"><h1 class="_16pg94n">Oops!</h1><p class="_166ju6w">We couldn&#x27;t find the Snack you&#x27;re looking for. Go to <a href="/">the homepage</a>.</p></div></div></div></div><script>(function(e,t){var n=e.amplitude||{_q:[],_iq:{}};var r=t.createElement("script")
+    ;r.type="text/javascript"
+    ;r.integrity="sha384-girahbTbYZ9tT03PWWj0mEVgyxtZoyDF9KVZdL+R53PP5wCY0PiVUKq0jeRlMx9M"
+    ;r.crossOrigin="anonymous";r.async=true
+    ;r.src="https://cdn.amplitude.com/libs/amplitude-7.2.1-min.gz.js"
+    ;r.onload=function(){if(!e.amplitude.runQueuedFunctions){
+    console.log("[Amplitude] Error: could not load SDK")}}
+    ;var i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)
+    ;function s(e,t){e.prototype[t]=function(){
+    this._q.push([t].concat(Array.prototype.slice.call(arguments,0)));return this}}
+    var o=function(){this._q=[];return this}
+    ;var a=["add","append","clearAll","prepend","set","setOnce","unset"]
+    ;for(var c=0;c<a.length;c++){s(o,a[c])}n.Identify=o;var u=function(){this._q=[]
+    ;return this}
+    ;var l=["setProductId","setQuantity","setPrice","setRevenueType","setEventProperties"]
+    ;for(var p=0;p<l.length;p++){s(u,l[p])}n.Revenue=u
+    ;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","enableTracking","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","groupIdentify","onInit","logEventWithTimestamp","logEventWithGroups","setSessionId","resetSessionId"]
+    ;function v(e){function t(t){e[t]=function(){
+    e._q.push([t].concat(Array.prototype.slice.call(arguments,0)))}}
+    for(var n=0;n<d.length;n++){t(d[n])}}v(n);n.getInstance=function(e){
+    e=(!e||e.length===0?"$default_instance":e).toLowerCase()
+    ;if(!n._iq.hasOwnProperty(e)){n._iq[e]={_q:[]};v(n._iq[e])}return n._iq[e]}
+    ;e.amplitude=n})(window,document);
+
+    amplitude.getInstance().init("e91121c19a5bdcccd852cd032dc911fe", null, { includeReferrer: true });</script><script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-53647600-5', {cookieDomain: 'auto', siteSpeedSampleRate: 100});ga('send', 'pageview');</script><script>(function () {const n = window,i = document,o = i.getElementsByTagName("script")[0],t = i.createElement("script");
+(t.src = "https://js.appetize.io/embed.js"), (t.async = 1), o.parentNode.insertBefore(t, o);
+const s = new Promise(function (e) {t.onload = function () {e();};});n.appetize = {getClient: function (...e) {
+return s.then(() => n.appetize.getClient(...e));},};})();</script><script src="https://cdn.jsdelivr.net/npm/babel-polyfill@6.26.0/dist/polyfill.min.js"></script><script src="/dist/app.bundle.js"></script></body></html>
